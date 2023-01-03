ba7fa99ab05d4f86d13069e342d318754a3f34f7
Ray Zana <rayzana280@gmail.com>
2023-01-03 17:23:46 -0500

    commit tree 358ab2a268d40eb1739a06f4b5dc3f7c21f45989

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+function superbowlWin(records){
+    const obj = records.find(checkWin);
+    if( obj !== undefined){
+        return obj.year  
+    }
+    else{
+        return undefined
+    }
+}
+
+function checkWin(element){
+    if(element.result === 'W'){
+        return true;
+    }
+}
